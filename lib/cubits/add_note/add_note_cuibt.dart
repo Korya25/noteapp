4dc8ae445242fecb43d49aa9c0a118dd315ed322
Korya25 <enthrj3@gmail.com>
2024-11-27 16:43:49 +0200

    register Adbator

--- lib/cubits/add_note/add_note_cuibt.dart
+++ lib/cubits/add_note/add_note_cuibt.dart
@@ -1,1 +1,8 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notesapp/cubits/add_note/add_note_state.dart';
 
+class AddNoteCuibt extends Cubit<AddNoteState> {
+  AddNoteCuibt() : super(AddNoteIntial());
+
+  addNote() {}
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notesapp/constants/key_word.dart';
 import 'package:notesapp/cubits/theme_cubit/theme_cuibt.dart';
+import 'package:notesapp/models/note_model.dart';
 import 'package:notesapp/screens/notes_view.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(
     BlocProvider(
       create: (context) {
